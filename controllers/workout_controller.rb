4168df7075edca6e9a2c55084477c94a7e58b553
lesliec <leslie.cayas@gmail.com>
2021-06-14 21:10:34 +1000

    can add exercise to workout

--- controllers/workout_controller.rb
+++ controllers/workout_controller.rb
@@ -16,11 +16,13 @@ get '/workouts/create' do       # Read CREATE form
 end
 
 post '/workouts' do
+  insert_query = "INSERT INTO workouts(user_id, workout_name) VALUES($1, $2)"
+  insert_sql_params = [current_user['id'], params[:workout_name]]
+  results = run_sql(insert_query, insert_sql_params)
 
-  query = "INSERT INTO workouts(user_id, workout_name) VALUES($1, $2)"
-  sql_params = [current_user['id'], params[:workout_name]]
-  results = run_sql(query, sql_params)
-  redirect "/"
+  select_query = "SELECT * FROM workouts WHERE id = (SELECT max(id) FROM workouts)"
+  select_results = run_sql(select_query)
+  redirect "/workouts/#{select_results[0]['id']}"
 end
 
 get '/workouts/:id' do |id|     # Read individual selected workout
@@ -28,6 +30,22 @@ get '/workouts/:id' do |id|     # Read individual selected workout
   erb :'workouts/show_workout'
 end
 
+get '/workouts/:id/add' do |id| 
+  query = "SELECT * FROM workouts WHERE id = $1"
+  sql_params = [id]
+  results = run_sql(query, sql_params)
+
+  erb :'/workouts/add_exercise_to_workout', locals: {individual_workout: results[0]}
+end
+
 post '/workouts/:id' do |id|
+  query = "INSERT INTO exercises(workout_id, name, image_url, weight, reps, sets) VALUES($1,$2,$3,$4,$5,$6)"
+  sql_params = [id, params[:exercise], params[:image_url], params[:weight], params[:reps], params[:sets]]
+  results = run_sql(query, sql_params)
+  redirect "/workouts/#{id}"
+end
+
+post '/workouts/:workout_title/:exercise_name' do |workout_title, exercise_name|
+
   
 end
